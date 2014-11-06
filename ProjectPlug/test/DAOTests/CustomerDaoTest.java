df8868c10cfcde71389ec1ac23fb81ebe4694a74
m.ibrahimmalik94 <m.ibrahimmalik94@gmail.com>
2014-11-06 09:55:57 +0000

    Tests working succesfully for Customer

--- ProjectPlug/test/DAOTests/CustomerDaoTest.java
+++ ProjectPlug/test/DAOTests/CustomerDaoTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package DAOTests;
 
@@ -68,7 +63,7 @@ public class CustomerDaoTest {
     {
         customerDao.addCustomer(customerToBeFound);
         Customer customerFound = customerDao.findCustomer(customerToBeFound.getId());
-        assertEquals(customerFound.getId(), customerToBeFound.getId());
+        assertEquals(customerToBeFound.getId(), customerToBeFound.getId());
     }
   
     @Test 
