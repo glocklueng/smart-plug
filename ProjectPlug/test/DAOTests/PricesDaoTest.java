0fda3a9fafb5ab3599413e53b3862bac6e264f88
m.ibrahimmalik94 <m.ibrahimmalik94@gmail.com>
2014-11-06 12:15:42 +0000

    Tests for PricesDao and TransactionDao by Team Morten and Ibrahim

--- ProjectPlug/test/DAOTests/PricesDaoTest.java
+++ ProjectPlug/test/DAOTests/PricesDaoTest.java
@@ -7,6 +7,8 @@
 package DAOTests;
 
 
+import DAO.PricesDao;
+import Model.Prices;
 import org.junit.After;
 import org.junit.AfterClass;
 import static org.junit.Assert.*;
@@ -15,20 +17,32 @@ import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 
+
 /**
  *
  * @author Dan
  */
 public class PricesDaoTest {
+    PricesDao pricesDao;
+    Prices pricesToBeAdded;
+    Prices pricesToBeFound;
+    Prices pricesToBeDeleted;
+ public PricesDaoTest() {
+        
+    }
     
     @Before
     public void setUp() {
-       
+       pricesDao = new PricesDao();
+       pricesToBeAdded = new Prices ("Brøndby", 3.5, 6.9);
+       pricesToBeFound = new Prices ("FCK", 3.0, 6.5);
+       pricesToBeDeleted = new Prices ("Pakistan", 3.5, 999.9);
     }
     
     @After
     public void tearDown() {
-       
+       pricesDao.deletePrices(pricesToBeAdded.getLocation());
+       pricesDao.deletePrices(pricesToBeFound.getLocation());
     }
 
     /**
@@ -36,17 +50,24 @@ public class PricesDaoTest {
      */
     @Test
     public void test1addPrices(){
+        int value = pricesDao.addPrices(pricesToBeAdded);
+        assertEquals(1,value);
     }
    
     
     @Test
     public void test2findPrices()
     {
-        
+        pricesDao.addPrices(pricesToBeFound);
+        Prices priceFound = pricesDao.findPrices(pricesToBeFound.getLocation());
+        assertEquals(pricesToBeFound.getLocation(), priceFound.getLocation());
     }
   
     @Test 
     public void test3deletePrices()
     {  
+        pricesDao.addPrices(pricesToBeDeleted);
+        int value = pricesDao.deletePrices(pricesToBeDeleted.getLocation());
+        assertEquals(1,value);
     }
 }
--- ProjectPlug/test/DAOTests/TransactionDaoTest.java
+++ ProjectPlug/test/DAOTests/TransactionDaoTest.java
@@ -3,10 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package DAOTests;
 
-
+import DAO.TransactionDao;
+import Model.Transaction;
 import org.junit.After;
 import org.junit.AfterClass;
 import static org.junit.Assert.*;
@@ -14,37 +14,58 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
+
 /**
  *
- * @author Dan
+ * @author Morten + Ibrahim
  */
 public class TransactionDaoTest {
+
+    TransactionDao transactionDao;
+    Transaction transactionToBeAdded;
+    Transaction transactionToBeFound;
+    Transaction transactionToBeDeleted;
+
+    public TransactionDaoTest() {
+    }
+
     @Before
     public void setUp() {
-       
+
+        transactionDao = new TransactionDao();
+        transactionToBeAdded = new Transaction(444, 333, 333, "2010-10-10 10:10:10", "Amsterdam", "blackberry", 40.0);
+        transactionToBeFound = new Transaction(445, 333, 333, "2010-10-10 10:10:10", "Amsterdam", "blackberry", 40.0);
+        transactionToBeDeleted = new Transaction(446, 333, 333, "2010-10-10 10:10:10", "Amsterdam", "blackberry", 40.0);
+
     }
-    
+
     @After
     public void tearDown() {
-       
+        transactionDao.deleteTransaction(transactionToBeAdded.getTransactionID());
+        transactionDao.deleteTransaction(transactionToBeFound.getTransactionID());
     }
 
     /**
      * Test of get Customer by id method, of class CsustomerDao.
      */
     @Test
-    public void test1addPrices(){
+    public void test1addPrices() {
+        int value = transactionDao.addTransaction(transactionToBeAdded);
+        assertEquals(1, value);
     }
-   
-    
+
     @Test
-    public void test2findPrices()
-    {
-        
+    public void test2findPrices() {
+        transactionDao.addTransaction(transactionToBeFound);
+        Transaction transactionFound = transactionDao.findTransactions(transactionToBeFound.getTransactionID());
+        assertEquals(transactionToBeFound.getTransactionID(), transactionFound.getTransactionID());
+
     }
-  
-    @Test 
-    public void test3deletePrices()
-    {  
+
+    @Test
+    public void test3deletePrices() {
+        transactionDao.addTransaction(transactionToBeDeleted);
+        int value = transactionDao.deleteTransaction(transactionToBeDeleted.getTransactionID());
+        assertEquals(value, 1);
     }
 }
