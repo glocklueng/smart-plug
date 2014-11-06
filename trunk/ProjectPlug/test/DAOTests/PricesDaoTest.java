6b7560f1c1f12b7216f7d893161047392c1dc446
dancoconetu@gmail.com <dancoconetu@gmail.com@da05afd8-26dc-60e5-2e96-4e692c01bff3>
2014-11-06 08:59:53 +0000

    Adding frame for more testing: Prices and Transaction

---
+++ trunk/ProjectPlug/test/DAOTests/PricesDaoTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package DAOTests;
+
+
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ *
+ * @author Dan
+ */
+public class PricesDaoTest {
+    
+    @Before
+    public void setUp() {
+       
+    }
+    
+    @After
+    public void tearDown() {
+       
+    }
+
+    /**
+     * Test of get Customer by id method, of class CsustomerDao.
+     */
+    @Test
+    public void test1addPrices(){
+    }
+   
+    
+    @Test
+    public void test2findPrices()
+    {
+        
+    }
+  
+    @Test 
+    public void test3deletePrices()
+    {  
+    }
+}
