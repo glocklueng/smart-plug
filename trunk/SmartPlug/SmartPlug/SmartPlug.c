a018987ee551eaf5d3fca613d44f11f082ef0ca0
dancoconetu@gmail.com <dancoconetu@gmail.com@da05afd8-26dc-60e5-2e96-4e692c01bff3>
2014-11-10 12:18:13 +0000

    Importing nothing

---
+++ trunk/SmartPlug/SmartPlug/SmartPlug.c
@@ -0,0 +1,17 @@
+/*
+ * SmartPlug.c
+ *
+ * Created: 10-Nov-14 1:13:39 PM
+ *  Author: Dan
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
