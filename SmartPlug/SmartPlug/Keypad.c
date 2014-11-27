1c3226e47ac428e64082526d925bd3f297a6a9c0
dancoconetu <dancoconetu@gmail.com>
2014-11-27 07:25:11 +0000

    Keypad still not working perfectly. Implementing the state machine in the main class SmartPlug.c

--- SmartPlug/SmartPlug/Keypad.c
+++ SmartPlug/SmartPlug/Keypad.c
@@ -7,14 +7,17 @@
 #include <avr/io.h>
 #include "Keypad.h"
 #include "uart.h"
+#include <util/delay.h>
+
 char key_table[4][4]= {{'F','E','D','C'},
 					   {'3','6','9','B'},
                        {'2','5','8','0'},
                        {'1','4','7','A'}};
-unsigned char keyPressed= 0;
+extern volatile int keyPressed;
 void initKeypad()
 {
 	DDRC|= (1<<DECODE1) | (1<<DECODE2);
+	
 }
 
 enum state1
@@ -29,15 +32,17 @@ enum state1
 	};
 state1=idle; 
 int count=1;
+unsigned char buffer[10];
+
 
 
 
 
 
 unsigned char RawKeyPressed()
-{	char temp1 = PIND&(1<<ENCODE1)>>6;
-	char temp2 = PIND&(1<<ENCODE2)>>6;
-	return temp1|temp2 |(PINB & (1<<PB2)); //0b000000q1q0 - the values of the output are in bit 0(q0),bit 1(q1)
+{   char temp1 = (PIND&(1<<ENCODE1))>>6;
+	 char temp2 = (PIND&(1<<ENCODE2))>>6;
+	return temp1|temp2; //0b000000q1q0 - the values of the output are in bit 0(q0),bit 1(q1)
 }
 
 void setRow(char cnt)
@@ -58,26 +63,27 @@ char scanKeyPad()
 	{
 		case idle: {if (count==5) 
 						count=1;
-					 PORTC&=~(1<<DECODE2); PORTC&=~(1<<DECODE1);
-					if ((PINB& (1<<PB2)) !=0) {
+					 setRow(count-1);
+					if ((PINB& (1<<PB2)) !=0 && ms>30 ) {
 						
 						state1=keyPresses;
+						ms=0;
 					}
 					else
 					{
 						state1=idle;
 						count++;
 					}
-					USART_Transmit('a');
+					
 					break;
 					}
-		case keyPresses: temp = RawKeyPressed(); state1= debounce; ms=0; USART_Transmit('b');break;
-		case debounce: if((ms= 30) && (temp==RawKeyPressed())) state1= getKey; else state1= debounce; USART_Transmit('c'); break;
-		case getKey: state1= waitRelease; key=findKey(count,temp); keyFound=1;USART_Transmit('d'); break;
-		case waitRelease: PORTB ^= (1<<PB0); if (((PINB& (1<<PB2)) !=0)) {state1= releasedDebounce; ms=0;} else {state1=released;}USART_Transmit('e');break ;
-		case releasedDebounce: if((ms>= 100) && ((PINB& (1<<PB2)) ==0)) {state1= released;USART_Transmit('f');} else { state1= releasedDebounce;USART_Transmit('k');} break;
-		case released:USART_Transmit('g'); state1=idle; count=1; 
-		default: state1=idle; count=1;
+		case keyPresses:  state1= debounce;  break;
+		case debounce: if((ms==30) ) {   state1= getKey;} else state1= debounce;  break;
+		case getKey: temp = RawKeyPressed(); state1= waitRelease; key=findKey(count,temp); keyFound=1; break;
+		case waitRelease:  if (keyPressed==1) {state1= releasedDebounce;} else {state1=released;}break ;
+		case releasedDebounce: if((ms>= 100) && (keyPressed ==0)) {state1= released;} else { state1= releasedDebounce;} break;
+		case released: state1=idle; count=1; break;
+		default: state1=idle; count=1; break;
 	}	
 	return keyFound;
 
@@ -87,13 +93,54 @@ char findKey(char row, char temp)
 	char key_d;
 	switch (temp)
 	{
-		case 3<<0 : key_d= key_table[row-1][3]; break;
-		case 2<<0 : key_d= key_table[row-1][2]; break;
-		case 1<<0 : key_d= key_table[row-1][1]; break;
-		case 0<<0 : key_d= key_table[row-1][0]; break;
+		case 0x03 : key_d= key_table[row-1][0]; break;
+		case 0x02 : key_d= key_table[row-1][1]; break;
+		case 0x01 : key_d= key_table[row-1][2]; break;
+		case 0x00 : key_d= key_table[row-1][3]; break;
 		default: break;
 	}
 	return key_d;
+}
+
+
+void timer1Init()
+{
+	TCNT1H= (-39)>>8;// the high byte
+	TCNT1L= (-39)&0xFF; //overflow after 31250 clocks
+	
+	TCCR1A = 0x00; //normal mode
+	TCCR1B= 0x04; // internal clock set to 1:256;
+	
+	//enable Timer 1 on the Timermask
+	
+}
+void int2Init()
+{
+	GICR|=(1<<INT2);
+	MCUCSR|= (1<< ISC2);
+	GIFR|=(1<<ISC2);
+}
+
+ISR(TIMER1_OVF_vect	){
+	ms++;
+	TCNT1H= (-39)>>8;// the high byte
+	TCNT1L= (-39)&0xFF;
+	//PORTB ^= (1<<PB0);
+}
+
+ISR(INT2_vect){
+	
+	keyPressed=(keyPressed+1)%2;
+	if (keyPressed==1)
+	{
+		MCUCSR&=~(1<<ISC2); // switch on falling edge the interrupt
+	}
+	else
+	{
+		MCUCSR|= (1<< ISC2);// switch on the rising edge the interrupt
+	}
+	PORTB^=(1<<PB0);
+	TIMSK|= (1<<TOIE1);
 }
 
 
