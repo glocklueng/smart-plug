8d932372bf0f0856522a4b8561c2bc6f8bc1f01e
dancoconetu <dancoconetu@gmail.com>
2015-01-12 08:41:16 +0000

    Serial Communication added

---
+++ ProjectPlug/src/Controller/StartSerialCommunication.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Controller;
+
+import SerialCommunication.CityBikePacket;
+import SerialCommunication.SerialTransceiver;
+import java.util.TooManyListenersException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Dan
+ */
+public class StartSerialCommunication {
+    public static void main(String[] args)
+    {
+           RFIDEventManagerSimple rFIDEventManagerSimple = new RFIDEventManagerSimple();
+        //Construct another SerialTransceiver for the RFIDEventManager
+     SerialTransceiver rFIDEventManagerTransceiver = new SerialTransceiver(new CityBikePacket(), rFIDEventManagerSimple);
+    
+    rFIDEventManagerSimple.setTransmitter(rFIDEventManagerTransceiver);
+            
+            try {
+                System.out.println("openning port");
+                rFIDEventManagerSimple.openPort();  
+                System.out.println("after port is openned");
+            } catch (TooManyListenersException ex) {
+                Logger.getLogger(StartSerialCommunication.class.getName()).log(Level.SEVERE, null, ex);
+                System.out.println("Error");
+            }
+    }
+}
